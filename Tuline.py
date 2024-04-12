fca231d6e2cbe416ccb7054c1daac27204f42460
MyiahS1276 <myiah.stubbs@ufl.edu>
2024-04-12 18:15:37 -0400

    Myiah Stubbs - added decoder

--- Tuline.py
+++ Tuline.py
@@ -3,6 +3,16 @@
 def encode(password):
     return ''.join(str((int(char) + 3) % 10) for char in password)
 
+def decode(encodedpass):
+    ogpass = ''
+    for item in encodedpass:
+        a = int(item) - 3
+        if a < 0:
+            ogpass += str(a + 10)
+        else:
+            ogpass += str(a)
+    return ogpass
+
 
 def main():
     encoded_password = None
@@ -11,6 +21,7 @@ def main():
         print("Menu")
         print("-------------")
         print("1. Encode")
+        print("2. Decode")
         print("3. Quit")
         option = input("Please enter an option: ")
 
@@ -21,7 +32,12 @@ def main():
                 print("Your password has been encoded and stored!")
             else:
                 print("Invalid password. Please make sure it is an 8-digit number.")
-
+        elif option == "2":
+            if encoded_password:
+                decoded_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+            else:
+                print("No password available to decode.")
         elif option == "3":
             print("Exiting program.")
             break
